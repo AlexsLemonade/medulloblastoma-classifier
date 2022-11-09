fa0d90db2448e056cf69724944daa41d00497b1a
Steven Foltz <stevenmasonfoltz@gmail.com>
2022-11-09 11:15:22 -0500

    Add OpenPBTA and others

--- 00-download_data.sh
+++ 00-download_data.sh
@@ -19,7 +19,7 @@ mkdir -p $data
 
 # GSE124814 is a series containing several datasets.
 # We can use the harmonized metadata from the series but will download each
-# dataset independently from refine.bio (and E-MTAB-292 from ArrayExpress).
+# dataset independently from refine.bio, except E-MTAB-292.
 
 if [[ -d $data/GSE124814 ]]; then
   
@@ -30,12 +30,11 @@ else
   mkdir -p $data/GSE124814
   GSE124814_sample_descriptions_url="https://ftp.ncbi.nlm.nih.gov/geo/series/GSE124nnn/GSE124814/suppl/GSE124814_sample_descriptions.xlsx"
   GSE124814_sample_descriptions_basename=$(basename $GSE124814_sample_descriptions_url)
-  curl -o $data/GSE124814/$GSE124814_sample_descriptions_basename --silent $GSE124814_sample_descriptions_url
   
-fi
+  echo Downloading metadata for series GSE124814...
+  curl -o $data/GSE124814/$GSE124814_sample_descriptions_basename --silent $GSE124814_sample_descriptions_url
 
-# re-run metadata processing on GSE124814
-Rscript -e "rmarkdown::render('GSE124814_metadata.Rmd')"
+fi
 
 # get medulloblastoma studies identified by GSE124814
 while read accession; do
@@ -46,8 +45,9 @@ while read accession; do
 
   elif [[ $accession == GSE* ]]; then
   
-    echo Downloading $accession
+    echo Downloading $accession...
     refinebio create-token -s
+    
     refinebio download-dataset \
       --email-address steven.foltz@ccdatalab.org \
       --path ${data}/${accession}.zip \
@@ -58,19 +58,12 @@ while read accession; do
 
     unzip -d ${data}/${accession} ${data}/${accession}.zip && rm -f ${data}/${accession}.zip
     
-    sleep 5
+    sleep 1
     
   elif [[ $accession == "E-MTAB-292" ]]; then
   
-    echo Downloading $accession
-    mkdir -p $data/$accession
-    idf_url="https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-292/E-MTAB-292.idf.txt"
-    sdrf_url="https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-292/E-MTAB-292.sdrf.txt"
-    genex_url="https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-292/E-MTAB-292.processed.1.zip/rma-gene-core.summary.txt"
-    curl -o $data/$accession/$(basename $idf_url) --silent $idf_url
-    curl -o $data/$accession/$(basename $sdrf_url) --silent $sdrf_url
-    curl -o $data/$accession/$(basename $genex_url) --silent $genex_url
-  
+    echo Skipping over E-MTAB-292.
+    
   else
   
     echo $accession does not start with GSE or equal E-MTAB-292...
@@ -89,14 +82,51 @@ if [[ -d $data/GSE164677 ]]; then
   
 else
 
-  echo Downloading GSE164677
+  echo Downloading GSE164677...
   mkdir $data/GSE164677
+  
   GSE164677_url="https://ftp.ncbi.nlm.nih.gov/geo/series/GSE164nnn/GSE164677/suppl/GSE164677_Asian_MB_RNA-seq.txt.gz"
-  curl -o $data/GSE164677/$(basename GSE164677_url) --silent $GSE164677_url
-  sleep 5
+  GSE164677_basename=$(basename $GSE164677_url)
+  
+  curl -o $data/GSE164677/$GSE164677_basename --silent $GSE164677_url
+  
+fi
+
+################################################################################
+# OpenPBTA
+################################################################################
+
+if [[ -d $data/OpenPBTA ]]; then
+
+  echo Data for OpenPBTA already exists and will not be downloaded.
+
+else
+
+  echo Downloading OpenPBTA...
+  mkdir $data/OpenPBTA
+  
+  OpenPBTA_release="release-v22-20220505"
+  
+  OpenPBTA_metadata_url="https://s3.amazonaws.com/d3b-openaccess-us-east-1-prd-pbta/data/$OpenPBTA_release/pbta-histologies.tsv"
+  OpenPBTA_genex_stranded_url="https://s3.amazonaws.com/d3b-openaccess-us-east-1-prd-pbta/data/$OpenPBTA_release/pbta-gene-expression-rsem-tpm.stranded.rds"
+  OpenPBTA_genex_polya_url="https://s3.amazonaws.com/d3b-openaccess-us-east-1-prd-pbta/data/$OpenPBTA_release/pbta-gene-expression-rsem-tpm.polya.rds"
+  OpenPBTA_metadata_basename=$(basename $OpenPBTA_metadata_url)
+  OpenPBTA_genex_stranded_basename=$(basename $OpenPBTA_genex_stranded_url)
+  OpenPBTA_genex_polya_basename=$(basename $OpenPBTA_genex_polya_url)
+  
+  curl -o $data/OpenPBTA/$OpenPBTA_metadata_basename --silent $OpenPBTA_metadata_url
+  curl -o $data/OpenPBTA/$OpenPBTA_genex_stranded_basename --silent $OpenPBTA_genex_stranded_url
+  curl -o $data/OpenPBTA/$OpenPBTA_genex_polya_basename --silent $OpenPBTA_genex_polya_url
 
 fi
 
+################################################################################
+# md5sum of downloaded data
+################################################################################
+
+# As needed, recreate data/md5_check_sums.tsv by running this command:
+# find data -type f | grep -v "json\|md5" | xargs md5sum > data/md5_check_sums.tsv
+
 # check md5 sums of downloaded files
-#echo Checking md5 sums of downloaded files ...
-#md5sum --check --quiet data/md5_check_sums.tsv
+echo Checking md5 sums of downloaded files...
+md5sum --check --quiet data/md5_check_sums.tsv && echo All good!
