bc427b71a2078d50f3caa8a60002bd96ad196b07
Steven Foltz <stevenmasonfoltz@gmail.com>
2022-11-17 12:08:28 -0500

    Use more general input file to define actions based on data sources

--- 00-download_data.sh
+++ 00-download_data.sh
@@ -2,6 +2,9 @@
 #
 # Steven Foltz
 # November 2022
+#
+# Usage: 00_download_data.sh data/data_sources.tsv
+# where data/data_sources.tsv is a TSV file with accession, data_source, and url columns
 
 #!/bin/bash
 set -euo pipefail
@@ -11,117 +14,70 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # set data directory
 data="data"
-mkdir -p $data
 
-################################################################################
-# GSE124814
-################################################################################
+# set input data sources file
+data_sources_file=$1
 
-# GSE124814 is a series containing several datasets.
-# We can use the harmonized metadata from the series but will download each
-# dataset independently from refine.bio, except E-MTAB-292.
+while read accession data_source url; do
 
-if [[ -d $data/GSE124814 ]]; then
-  
-  echo Metadata for series GSE124814 already exists and will not be downloaded.
+  # skip over any header lines starting with hash
+  if [[ $accession =~ ^#.* ]] && continue
 
-else
-
-  mkdir -p $data/GSE124814
-  GSE124814_sample_descriptions_url="https://ftp.ncbi.nlm.nih.gov/geo/series/GSE124nnn/GSE124814/suppl/GSE124814_sample_descriptions.xlsx"
-  GSE124814_sample_descriptions_basename=$(basename $GSE124814_sample_descriptions_url)
-  
-  echo Downloading metadata for series GSE124814...
-  curl -o $data/GSE124814/$GSE124814_sample_descriptions_basename --silent $GSE124814_sample_descriptions_url
-
-fi
-
-# get medulloblastoma studies identified by GSE124814
-while read accession; do
-
-  if [[ -d $data/$accession ]]; then
-  
-    echo Data for $accession already exists and will not be downloaded.
-
-  elif [[ $accession == GSE* ]]; then
-  
-    echo Downloading $accession...
-    refinebio create-token -s
-    
-    refinebio download-dataset \
-      --email-address steven.foltz@ccdatalab.org \
-      --path ${data}/${accession}.zip \
-      --experiments $accession \
-      --aggregation EXPERIMENT \
-      --transformation NONE \
-      --skip-quantile-normalization True
-
-    unzip -d ${data}/${accession} ${data}/${accession}.zip && rm -f ${data}/${accession}.zip
-    
-    sleep 1
-    
-  elif [[ $accession == "E-MTAB-292" ]]; then
+  if [[ $accession == "E-MTAB-292" ]]; then
   
     # E-MTAB-292 is part of ArrayExpress.
     # We can come back to this dataset in the future.
     # For now it is not clear how to easily download processed expression data.
     echo Skipping over E-MTAB-292.
-    
-  else
+
+  elif [[ $data_source == refine\.bio ]]; then
+
+    if [[ -d $data/$accession ]]; then
   
-    echo $accession does not start with GSE or equal E-MTAB-292...
+      echo Data for $accession already exists and will not be downloaded.
+  
+    else
     
+      echo Downloading $accession from refine.bio...
+      refinebio create-token -s
+    
+      refinebio download-dataset \
+        --email-address steven.foltz@ccdatalab.org \
+        --path ${data}/${accession}.zip \
+        --experiments $accession \
+        --aggregation EXPERIMENT \
+        --transformation NONE \
+        --skip-quantile-normalization True
+
+      unzip -d ${data}/${accession} ${data}/${accession}.zip && rm -f ${data}/${accession}.zip
+    
+      sleep 1
+      
+    fi
+
+  elif [[ $data_source == url ]]; then
+
+    mkdir -p $data/$accession
+    url_basename=$(basename $url)
+    file_name=$data/$accession/$url_basename
+    
+    if [[ -f $file_name ]]; then
+      
+      echo File $file_name already exists and will not be downloaded.
+  
+    else
+    
+      echo Downloading $accesion $url_basename...
+      
+      curl -o $file_name --silent $url
+      
+      sleep 1
+      
+    fi
+  
   fi
-  
-done < $data/GSE124814_experiment_accessions.tsv
-
-################################################################################
-# GSE164677 -- not in refine.bio
-################################################################################
-
-if [[ -d $data/GSE164677 ]]; then
-
-  echo Data for GSE164677 already exists and will not be downloaded.
-  
-else
-
-  echo Downloading GSE164677...
-  mkdir $data/GSE164677
-  
-  GSE164677_url="https://ftp.ncbi.nlm.nih.gov/geo/series/GSE164nnn/GSE164677/suppl/GSE164677_Asian_MB_RNA-seq.txt.gz"
-  GSE164677_basename=$(basename $GSE164677_url)
-  
-  curl -o $data/GSE164677/$GSE164677_basename --silent $GSE164677_url
-  
-fi
-
-################################################################################
-# OpenPBTA
-################################################################################
-
-if [[ -d $data/OpenPBTA ]]; then
-
-  echo Data for OpenPBTA already exists and will not be downloaded.
-
-else
-
-  echo Downloading OpenPBTA...
-  mkdir $data/OpenPBTA
-  
-  OpenPBTA_release="release-v22-20220505"
-  
-  OpenPBTA_metadata_url="https://s3.amazonaws.com/d3b-openaccess-us-east-1-prd-pbta/data/$OpenPBTA_release/pbta-histologies.tsv"
-  OpenPBTA_genex_stranded_url="https://s3.amazonaws.com/d3b-openaccess-us-east-1-prd-pbta/data/$OpenPBTA_release/pbta-gene-expression-rsem-tpm.stranded.rds"
-  OpenPBTA_genex_polya_url="https://s3.amazonaws.com/d3b-openaccess-us-east-1-prd-pbta/data/$OpenPBTA_release/pbta-gene-expression-rsem-tpm.polya.rds"
-  OpenPBTA_metadata_basename=$(basename $OpenPBTA_metadata_url)
-  OpenPBTA_genex_stranded_basename=$(basename $OpenPBTA_genex_stranded_url)
-  OpenPBTA_genex_polya_basename=$(basename $OpenPBTA_genex_polya_url)
-  
-  curl -o $data/OpenPBTA/$OpenPBTA_metadata_basename --silent $OpenPBTA_metadata_url
-  curl -o $data/OpenPBTA/$OpenPBTA_genex_stranded_basename --silent $OpenPBTA_genex_stranded_url
-  curl -o $data/OpenPBTA/$OpenPBTA_genex_polya_basename --silent $OpenPBTA_genex_polya_url
-
-fi
+    
+done < $data_sources_file
 
 ################################################################################
 # md5sum of downloaded data
