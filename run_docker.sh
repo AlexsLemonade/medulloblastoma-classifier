e2293707ea18a9eeb765cff6793131775ea11c0c
Steven Foltz <stevenmasonfoltz@gmail.com>
2022-11-23 20:36:01 -0500

    Script to launch docker container

---
+++ run_docker.sh
@@ -0,0 +1,14 @@
+# script to get docker image up and running
+
+sudo snap install docker
+
+sleep 3 && sudo chmod 666 /var/run/docker.sock
+
+docker pull envest/mbssp:R-4.2.2
+
+docker run -d --rm --mount type=volume,dst=/home/rstudio,volume-driver=local,volume-opt=type=none,volume-opt=o=bind,volume-opt=device=/home/ubuntu -e PASSWORD=onecupatatime envest/mbssp:R-4.2.2
+
+container_id=$(docker ps -ql)
+
+docker exec -it $container_id bash
+
