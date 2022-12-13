e9bcf73fa8ad1ba67469c0c54cf3a3e280bef9d6
cbethell <chantebethell@gmail.com>
2022-12-13 14:34:32 -0600

    fix column headers and isolate columns with double expression values

--- 02-clean-pseudobulk-expression.R
+++ 02-clean-pseudobulk-expression.R
@@ -16,12 +16,21 @@ expression_files <- list.files(file.path("data", "GSE119926"), "GSM")
 # read in individual expression files
 expression_df_list <- list()
 for (file in expression_files[-c(4:5, 25, 28, 33)]) {
-  file_df <-  data.frame(readr::read_tsv(file.path("data", "GSE119926", file))) %>%
-    distinct()
-  
-  # set gene names as rownames  
-  rownames(file_df) <- file_df[,1]
-  file_df <- file_df[,-1]
+  file_df <-  data.frame(readr::read_tsv(file.path("data", "GSE119926", file)))
+  column_names <- c("gene", colnames(file_df))
+  file_df$extra <- file_df[,length(colnames(file_df))]
+  # fix columns with more than one value
+  file_df <- file_df %>%
+    dplyr::mutate(second_to_last_column = gsub("\t.*$", "", file_df[,ncol(file_df)-1]),
+                  last_column = gsub(".*\t", "", file_df[,ncol(file_df)-2]))
+  # remove unnecessary columns
+  file_df <- file_df[ , -((ncol(file_df) - 3):(ncol(file_df) - 2))] 
+
+  # set correct column names 
+  colnames(file_df) <- column_names
+  # set gene names as rownames
+  file_df <- file_df %>%
+    column_to_rownames("gene")
   
   # add to list of expression data frames
   expression_df_list[[file]] <- file_df 
