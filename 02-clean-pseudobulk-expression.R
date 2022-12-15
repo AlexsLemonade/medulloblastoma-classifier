860640ba95fbd1e410cba964ff44fd41599e9652
cbethell <chantebethell@gmail.com>
2022-12-15 15:47:43 -0600

    Isolate TPM and average values across cells for each sample data frame

--- 02-clean-pseudobulk-expression.R
+++ 02-clean-pseudobulk-expression.R
@@ -22,3 +22,11 @@ expression_df_list <- purrr::map(expression_files,
                                  function(x)
                                    readr::read_tsv(x, skip = 1, col_names = FALSE) %>%
                                    tibble::column_to_rownames(var = "X1"))
+
+# revert log transformed-TPM values back to original TPM values -- working back
+# from the equation found at https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM3905406
+tpm_df_list <- lapply(expression_df_list, function(x) (2^((10+1)*x)))
+
+# average the TPM values across cells for each data frame
+average_tpm_list <- lapply(expression_df_list, function(x) rowMeans(x))
+names(average_tpm_list) <- pseudobulk_metadata$title
