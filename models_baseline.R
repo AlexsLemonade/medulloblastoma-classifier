28380a2abeaac9ce2fe840854692599646743758
Steven Foltz <stevenmasonfoltz@gmail.com>
2023-01-17 12:36:46 -0500

    Input/output file processing

--- models_baseline.R
+++ models_baseline.R
@@ -3,9 +3,29 @@
 # up to 100 rules (revise higher if models include close to that many)
 # repeat 10 times
 
+library(tidyverse)
+source("utils/modeling.R")
+
+processed_data_dir <- here::here("processed_data")
 models_dir <- here::here("models")
 models_list_output_filepath <- file.path(models, "baseline.rds")
 
+genex_df_input_filepath <- file.path(processed_data_dir, "bulk_genex.tsv")
+metadata_df_input_filepath <- file.path(processed_data_dir, "bulk_metadata.tsv")
+
+# read in data
+genex_df <- read_tsv(genex_df_input_filepath) %>%
+  column_to_rownames(var = "gene")
+
+metadata_df <- read_tsv(metadata_df_input_filepath) %>%
+  filter(sample_accession %in% names(genex_df),
+         subgroup %in% c("G3", "G4", "SHH", "WNT"),
+         !is_duplicate)
+
+genex_df <- genex_df %>%
+  select(all_of(metadata_df$sample_accession))
+
+# run models
 models_list <- run_many_models(genex_df,
                                metadata_df,
                                model_types = c("ktsp", "rf", "mm2s", "lasso"),
@@ -15,5 +35,6 @@ models_list <- run_many_models(genex_df,
                                n_rules_min = 5,
                                n_rules_max = 100)
 
+# save models
 readr::write_rds(models_list,
                  file = models_list_output_filepath)
