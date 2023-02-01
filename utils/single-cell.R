04c06a26845a49b6fd1a1247a4d38a2cef3e641d
cbethell <chantebethell@gmail.com>
2023-02-01 13:48:36 -0600

    convert pseudobulk matrices to SCE objects and write to file

---
+++ utils/single-cell.R
@@ -0,0 +1,28 @@
+# Functions related to SingleCellExperiment operations
+#
+# Chante Bethell
+# February 2023
+
+convert_to_sce <- function(df_list, results_dir) {
+  # Purpose: Convert a list of data frame objects into SingleCellExperiment
+  # objects and write to file
+  
+  # Args:
+  #   df_list: the list of data frames to be converted into SingleCellExperiment
+  #            objects
+  #   results_dir: the output directory for storing each individual 
+  #                SingleCellExperiment RDS file
+  
+  # convert matrices to SingleCellExperiment objects
+  sce_list <- lapply(df_list, function(x) SingleCellExperiment::SingleCellExperiment(x))
+  
+  # define the output file names
+  output_filenames <- paste0(names(sce_list), "_sce.rds")
+  
+  # write each SingleCellExperiment object to file
+  for (i in 1:length(output_filenames)) {
+    write_rds(sce_list[[i]], file.path(results_dir, output_filenames[[i]]))
+  }
+  
+  return(sce_list)
+}
