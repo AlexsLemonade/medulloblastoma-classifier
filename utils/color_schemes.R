3e61e3e3ea8fd007cdf711e2066f69544684bf02
Steven Foltz <stevenmasonfoltz@gmail.com>
2023-04-14 09:56:45 -0400

    Add Okabe-Ito color scheme

---
+++ utils/color_schemes.R
@@ -0,0 +1,6 @@
+# Okabe-Ito is a color-blind friendly color scheme
+# Reference: Okabe, M., & Ito, K. (2008). Color universal design (CUD): How to make figures and presentations that are friendly to colorblind people. https://jfly.uni-koeln.de/color/#pallet (Original work published 2002)
+
+# palette.colors returns a named list, which must be unnamed to not match color names to category labels
+okabe_ito_colors <- unname(grDevices::palette.colors(palette = "Okabe-Ito"))
+
