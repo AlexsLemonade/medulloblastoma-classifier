d5611e984bf42dc50d44797d117a0fa7f02fa003
Steven Foltz <stevenmasonfoltz@gmail.com>
2023-04-20 12:18:58 -0400

    Flesh out convert_gene_names function

--- utils/convert_gene_names.R
+++ utils/convert_gene_names.R
@@ -1,4 +1,9 @@
-convert_gene_names <- function() {
+convert_gene_names <- function(genex_df,
+                               gene_column_before,
+                               gene_column_after,
+                               gene_map_df,
+                               map_from,
+                               map_to) {
   
   # Converts the gene column of a gene expression matrix to a different annotation style
   # e.g. from SYMBOL to ENSEMBL
@@ -9,27 +14,38 @@ convert_gene_names <- function() {
   #
   # Inputs
   #  genex_df: gene expression matrix with a gene name column
-  #  gene_column: name of the gene column (default: gene)
+  #  gene_column_before: name of the gene column before conversion
+  #  gene_column_after: name of the gene column after conversion
   #  gene_map_df: data frame mapping gene annotations across columns (one gene per row)
   #  map_from: the annotation style of the original gene column
   #  map_to: the annotation style of the new gene column
   #
   # Output
-  #  Gene expression matrix with a "gene" column, now with new gene annotation style
+  #  Gene expression matrix with a "gene" column
   
-  # check that gene_column is in genex_df
+  # check that gene_column_before is in genex_df
+  
+  if(!(gene_column_before %in% names(genex_df))) {
+  
+    stop("Gene column missing from gene expression matrix in convert_gene_names().")  
+    
+  }
   
   # check that map_from and map_to are in gene_map_df
   
+  if(!all(c(map_from, map_to) %in% names(gene_map_df))) {
+    
+    stop("Annotation styles missing from gene map in convert_gene_names().")
+    
+  }
+  
   genex_df |>
     dplyr::left_join(gene_map_df |> dplyr::select(map_from, map_to),
-                     by = c(gene_column = map_from)) |>
-    dplyr::filter(!duplicated(gene_column),
-                  !duplicated(map_to),
-                  !is.na(map_to)) |>
-    dplyr::select(-gene_column,
-                  gene = map_to,
-                  tidyselect::everything()) |>
-    return()
+                     by = setNames(map_from, gene_column_before)) |>
+    dplyr::filter(!duplicated(.data[[gene_column_before]]), # multi-mapped left to right
+                  !duplicated(.data[[map_to]]), # multi-mapped right to left
+                  !is.na(.data[[map_to]])) |> # no match left to right
+    dplyr::select(-dplyr::all_of(gene_column_before)) |> # remove old gene column
+    dplyr::relocate(!!gene_column_after := map_to) # rename and move new gene column to first position
   
 }
