707f7548ad4dc0a2f5f09bf0fa6adac6aef3a273
Steven Foltz <stevenmasonfoltz@gmail.com>
2023-05-11 12:33:23 -0400

    Run analysis notebook

--- 04-single_cell_experiments.R
+++ 04-single_cell_experiments.R
@@ -1,6 +1,13 @@
-# Add dimensionality reduction and clustering results to pseudobulk
-# SingleCellExperiment objects
+# Run experiments using pseudobulk and single-cell gene expression data
 #
-# Chante Bethell
-# February 2023
+# Chante Bethell, Steven Foltz
+# 2023
 
+# set up directories
+nb_dir <- here::here("analysis_notebooks")
+
+# notebook file paths
+test_pseudobulk_and_single_cells_filepath <- file.path(nb_dir, "test_pseudobulk_and_single_cells.Rmd")
+
+# Test pseudobulk and single cells
+rmarkdown::render(test_pseudobulk_and_single_cells_filepath)
