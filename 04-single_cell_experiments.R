9da55f1b8dd884baf32a9d52f30a57974e1a7672
envest <stevenmasonfoltz@gmail.com>
2024-10-30 18:47:05 +0000

    Update name of single cell notebook by removing predict and re-render the notebook

--- 04-single_cell_experiments.R
+++ 04-single_cell_experiments.R
@@ -9,7 +9,7 @@ nb_dir <- here::here("analysis_notebooks")
 
 # script file paths
 predict_pseudobulk_and_single_cells_filepath <- file.path(predict_dir, "predict_pseudobulk_and_single_cells.R")
-pseudobulk_and_single_cells_analysis_notebook_filepath <- file.path(nb_dir, "predict_pseudobulk_and_single_cells.Rmd")
+pseudobulk_and_single_cells_analysis_notebook_filepath <- file.path(nb_dir, "pseudobulk_and_single_cells.Rmd")
 
 # Predict pseudobulk and single cells; create plot data
 source(predict_pseudobulk_and_single_cells_filepath)
