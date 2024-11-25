498c04ff2db122e0a9c2e42a5e464eac43cc96fa
Steven Foltz <stevenmasonfoltz@gmail.com>
2024-11-25 15:21:57 -0500

    Render baseline model notebook

--- 03-bulk_experiments.R
+++ 03-bulk_experiments.R
@@ -8,6 +8,11 @@ nb_dir <- here::here("analysis_notebooks")
 
 # notebook file paths
 estimate_tumor_purity_filepath <- file.path(nb_dir, "ESTIMATE_tumor_purity.Rmd")
+baseline_models_filepath <- file.path(nb_dir, "baseline_models.Rmd")
 
 # ESTIMATE tumor purity
 rmarkdown::render(estimate_tumor_purity_filepath)
+
+# Run baseline models
+rmarkdown::render(baseline_models_filepath, params = list(create_models = TRUE))
+
