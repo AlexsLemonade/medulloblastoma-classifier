6d5038d76e2fe592c72f3426a402a94b2c846f05
envest <stevenmasonfoltz@gmail.com>
2025-02-13 01:44:53 +0000

    Source predict_baseline_models.R

--- 03-bulk_experiments.R
+++ 03-bulk_experiments.R
@@ -4,15 +4,18 @@
 # 2023
 
 # set up directories
+predict_dir <- here::here("predict")
 nb_dir <- here::here("analysis_notebooks")
 
 # notebook file paths
 estimate_tumor_purity_filepath <- file.path(nb_dir, "ESTIMATE_tumor_purity.Rmd")
+predict_baseline_models_filepath <- file.path(predict_dir, "predict_baseline_models.R")
 baseline_models_filepath <- file.path(nb_dir, "baseline_models.Rmd")
 
 # ESTIMATE tumor purity
 #rmarkdown::render(estimate_tumor_purity_filepath)
 
 # Run baseline models
-rmarkdown::render(baseline_models_filepath, params = list(create_models = TRUE))
+source(predict_baseline_models_filepath)
+#rmarkdown::render(baseline_models_filepath, params = list(create_models = TRUE))
 
