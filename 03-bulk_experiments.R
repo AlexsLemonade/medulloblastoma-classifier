c9aa92c36e1c01c41b3ecc5296d9453bff5340ca
envest <stevenmasonfoltz@gmail.com>
2025-04-15 01:33:10 +0000

    Rename and uncomment scripts

--- 03-bulk_experiments.R
+++ 03-bulk_experiments.R
@@ -1,7 +1,6 @@
 # Run experiments using bulk gene expression data
 #
 # Steven Foltz
-# 2023
 
 # set up directories
 predict_dir <- here::here("predict")
@@ -10,12 +9,11 @@ nb_dir <- here::here("analysis_notebooks")
 # notebook file paths
 estimate_tumor_purity_filepath <- file.path(nb_dir, "ESTIMATE_tumor_purity.Rmd")
 predict_baseline_models_filepath <- file.path(predict_dir, "predict_baseline_models.R")
-baseline_models_filepath <- file.path(nb_dir, "baseline_models.Rmd")
+bulk_analysis_filepath <- file.path(nb_dir, "bulk_analysis.Rmd")
 
 # ESTIMATE tumor purity
 #rmarkdown::render(estimate_tumor_purity_filepath)
 
 # Run baseline models
 source(predict_baseline_models_filepath)
-#rmarkdown::render(baseline_models_filepath, params = list(create_models = TRUE))
-
+rmarkdown::render(bulk_analysis_filepath, params = list(create_models = TRUE))
