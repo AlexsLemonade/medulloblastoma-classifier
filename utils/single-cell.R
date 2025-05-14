49f70a3e4030172bcec8fba3c7e07376f0687979
Jaclyn Taroni <19534205+jaclyn-taroni@users.noreply.github.com>
2025-05-14 15:40:21 -0400

    WIP: Skip classifying cells if they have too few genes detected

--- utils/single-cell.R
+++ utils/single-cell.R
@@ -118,6 +118,8 @@ test_single_cells <- function(sample_acc,
                               gene_map_df,
                               labels,
                               classifier,
+                              genes_in_classifier,
+                              prop_genes_detected = 0.25,
                               platform = "scRNA-seq") {
   # Applies prediction model to gene expression matrix
   #
@@ -127,6 +129,8 @@ test_single_cells <- function(sample_acc,
   #  metadata_df: metadata data frame (must include sample_accession, study, subgroup, and platform columns)
   #  labels: vector of possible sample labels (e.g., c("G3","G4","SHH","WNT"))
   #  classifier: classifier model (model class must be OnevsrestScheme_TSP for kTSP or rule_based_RandomForest for random forest)
+  #  genes_in_classifier: a vector of genes used in the rules-based classifier
+  #  prop_genes_detected: what proportion of genes used in the classifier need to be detected to move forward with prediction? (default: 0.25)
   #  platform: gene expression platform for this sample (default: scRNA-seq)
   #
   # Outputs:
@@ -173,13 +177,13 @@ test_single_cells <- function(sample_acc,
   # If the sample_subgroup vector is longer than 1, more than one row has this
   # sample accession, meaning that sample accession is not a unique identifier
   # in this metadata (and this should be addressed by the user)
-
+  
   if (length(sample_subgroup) != 1) {
-
+    
     stop(glue::glue("Sample accession ", sample_acc, " is not unique in scRNA metadata."))
-
+    
   }
-
+  
   # test_*() function requires a metadata file
   metadata_df_this_sample <- tibble::tibble(index = 1:n_cells,
                                             sample_accession = stringr::str_c(sample_acc,
@@ -188,6 +192,20 @@ test_single_cells <- function(sample_acc,
                                             study = sample_study,
                                             subgroup = sample_subgroup,
                                             platform = platform)
+  
+  # just genes in classifier
+  classifier_genex <- genex_df_this_sample[which(rownames(genex_df_this_sample) %in% genes_in_classifier), ]
+  # find the number of classifier-relevant genes detected
+  num_genes_detected <- apply(classifier_genex, 2, function(x) sum(x > 0))
+  # find cells to retain for prediction
+  cells_to_retain <- which((num_genes_detected / length(genes_in_classifier)) > prop_genes_detected)
+  # unclassified cell identifiers
+  unclassified_cell_ids <- colnames(genex_df_this_sample)[-cells_to_retain]
+  
+  # for prediction, only retain the cells above the threshold
+  genex_df_this_sample <- genex_df_this_sample[, cells_to_retain]
+  metadata_df_this_sample <- metadata_df_this_sample |>
+    dplyr::filter(sample_accession %in% colnames(genex_df_this_sample))
 
   # check_input_files() is sourced from utils/modeling.R. This function ensures
   #  the genex_df and metadata_df given to the test_*() function are properly
@@ -197,20 +215,69 @@ test_single_cells <- function(sample_acc,
 
   # predict the subgroup of each observation (individual cell) using given model
   if (model_type == "ktsp") {
-
+    
     test_object <- test_ktsp(genex_df_test = genex_df_this_sample,
                              metadata_df_test = metadata_df_this_sample,
                              classifier = classifier,
                              labels = labels)
+    
+    if (length(unclassified_cell_ids) > 0) {
+      
+      # build out a prediction matrix for the unclassified cells to append
+      # to test_object$model_output
+      pred_mat_unclassified <- data.frame(
+        cell_id = unclassified_cell_ids,
+        G3 = NA_real_,
+        G4 = NA_real_,
+        SHH = NA_real_,
+        WNT = NA_real_,
+        max_score = "Unclassified",
+        tie_flag = NA_character_
+      ) |>
+        tibble::column_to_rownames("cell_id")
+      
+      # tack on the unclassified cells
+      test_object$model_output <- data.frame(test_object$model_output) |>
+        dplyr::bind_rows(pred_mat_unclassified)
+
+    }
 
   } else if (model_type == "rf") {
 
     test_object <- test_rf(genex_df_test = genex_df_this_sample,
                            metadata_df_test = metadata_df_this_sample,
                            classifier = classifier)
-
+    
+    if (length(unclassified_cell_ids) > 0) {
+      # build out a prediction matrix for the unclassified cells to append to
+      # test_object$model_output
+      pred_mat_unclassified <- data.frame(
+        cell_id = unclassified_cell_ids,
+        G3 = NA_real_,
+        G4 = NA_real_,
+        SHH = NA_real_,
+        WNT = NA_real_
+      ) |>
+        tibble::column_to_rownames("cell_id")
+      
+      # tack on the unclassified cells
+      test_object$model_output <- data.frame(test_object$model_output) |>
+        dplyr::bind_rows(pred_mat_unclassified)
+    }
   }
 
+  if (length(unclassified_cell_ids) > 0) {
+    # make a data frame of the unclassified cells to rbind to the test object
+    pred_unclassified_df <- tibble::tibble(
+      sample_accession = unclassified_cell_ids,
+      predicted_labels = "Unclassified"
+    )
+    
+    # add unclassified cells & return
+    test_object$predicted_labels_df <- test_object$predicted_labels_df |>
+      dplyr::bind_rows(pred_unclassified_df)
+  }
+  
   return(test_object)
 
 }
