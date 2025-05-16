f6f58b2f3f84983ed80ead07e04d53d420420bd3
Jaclyn Taroni <19534205+jaclyn-taroni@users.noreply.github.com>
2025-05-16 14:39:07 -0400

    Vary the proportion of genes observed for filtering out cells

--- 05-single_cell_experiments.sh
+++ 05-single_cell_experiments.sh
@@ -13,9 +13,35 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # Set up directories
 predict_dir="predict"
 analysis_notebooks_dir="analysis_notebooks"
+results_dir="results/single_cells_filtered"
+mkdir -p $results_dir
 
 # Run prediction on single-cell and pseudobulk data
 Rscript "${predict_dir}/predict_pseudobulk.R"
 
 # Render notebook for pseudobulk analysis
 Rscript -e "rmarkdown::render('${analysis_notebooks_dir}/pseudobulk_analysis.Rmd')"
+
+# Vary the proportion of genes in a model that need to be observed to classify
+# an individual cell
+for prop_observed in 0 0.05 0.10 0.15 0.20 0.25 0.5; do
+  
+  # At each proportion observed value, run kTSP using two different models of
+  # filtering out cells
+  for filtering_type in gene rule; do
+  
+    Rscript "${predict_dir}/predict_single_cells.R" \
+      --output_file "${results_dir}/ktsp_${prop_observed}_${filtering_type}.tsv" \
+      --model_type ktsp \
+      --prop_observed ${prop_observed} \
+      --ktsp_mode ${filtering_type}
+      
+  done
+  
+  # Random forest models using gene filtering per proportion observed value
+  Rscript "${predict_dir}/predict_single_cells.R" \
+    --output_file "${results_dir}/rf_${prop_observed}.tsv" \
+    --model_type rf \
+    --prop_observed ${prop_observed}
+  
+done
