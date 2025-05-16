ced5fe3d966e7e0d52732b1ff6069dfa9a7b7885
Jaclyn Taroni <19534205+jaclyn-taroni@users.noreply.github.com>
2025-05-16 12:54:17 -0400

    Combine single-cell prediction into a single function (again)

--- utils/single-cell.R
+++ utils/single-cell.R
@@ -112,142 +112,48 @@ perform_graph_clustering <- function(sce,
 
 }
 
-test_rf_single_cells <- function(sample_acc,
-                                 sce_filepath,
-                                 metadata_df,
-                                 labels,
-                                 classifier,
-                                 genes_in_classifier,
-                                 prop_observed = 0.1,
-                                 platform = "scRNA-seq") {
+
+test_single_cells <- function(sample_acc,
+                              sce_filepath,
+                              metadata_df,
+                              labels,
+                              classifier,
+                              genes_in_classifier = NULL,
+                              rules_df = NULL,
+                              filtering_type = "rule",
+                              prop_observed = 0.1,
+                              platform = "scRNA-seq") {
   
-  stopifnot(
-    "Classifier type not rule_based_RandomForest" = (class(classifier) == "rule_based_RandomForest")
-  )
-  
-  # read in single cell experiment object
-  sce_object <- readr::read_rds(sce_filepath)
-  
-  # read in gene expression matrix
-  genex_df_this_sample <- SingleCellExperiment::counts(sce_object)
-  
-  # get number of cells to be tested
-  n_cells <- ncol(genex_df_this_sample)
-  
-  # set column names as the same sample accession across all cells (columns)
-  names(genex_df_this_sample) <- stringr::str_c(rep(sample_acc, n_cells),
-                                                1:n_cells,
-                                                sep = "_")
-  
-  # get subgroup of sample given
-  sample_subgroup <- metadata_df |>
-    dplyr::filter(sample_accession == sample_acc) |>
-    dplyr::pull(subgroup)
-  
-  # get the study of a given sample
-  sample_study <- metadata_df |>
-    dplyr::filter(sample_accession == sample_acc) |>
-    dplyr::pull(study)
-  
-  # If the sample_subgroup vector is longer than 1, more than one row has this
-  # sample accession, meaning that sample accession is not a unique identifier
-  # in this metadata (and this should be addressed by the user)
-  
-  if (length(sample_subgroup) != 1) {
+  # Detect classifier class and error-handling
+  if ( class(classifier) == "OnevsrestScheme_TSP" ) {
     
-    stop(glue::glue("Sample accession ", sample_acc, " is not unique in scRNA metadata."))
+    model_type <- "ktsp"
     
-  }
-  
-  # test_*() function requires a metadata file
-  metadata_df_this_sample <- tibble::tibble(index = 1:n_cells,
-                                            sample_accession = stringr::str_c(sample_acc,
-                                                                              1:n_cells,
-                                                                              sep = "_"),
-                                            study = sample_study,
-                                            subgroup = sample_subgroup,
-                                            platform = platform)
-  
-  # just genes in classifier
-  classifier_genex <- genex_df_this_sample[which(rownames(genex_df_this_sample) %in% genes_in_classifier), ]
-  # find the number of classifier-relevant genes detected
-  num_genes_detected <- apply(classifier_genex, 2, function(x) sum(x > 0))
-  # find cells to retain for prediction
-  cells_to_retain <- which(
-    (num_genes_detected / length(genes_in_classifier)) > prop_observed
-  )
-  # unclassified cell identifiers
-  unclassified_cell_ids <- colnames(genex_df_this_sample)[-cells_to_retain]
-  
-  # for prediction, only retain the cells above the threshold
-  genex_df_this_sample <- genex_df_this_sample[, cells_to_retain]
-  metadata_df_this_sample <- metadata_df_this_sample |>
-    dplyr::filter(sample_accession %in% colnames(genex_df_this_sample))
-  
-  # check_input_files() is sourced from utils/modeling.R. This function ensures
-  #  the genex_df and metadata_df given to the test_*() function are properly
-  #  formatted and consistent with each other
-  check_input_files(genex_df = genex_df_this_sample,
-                    metadata_df = metadata_df_this_sample)
-  
-  # If there are any cells retained, predict as normal
-  if (ncol(genex_df_this_sample) > 0) {
+  } else if ( class(classifier) == "rule_based_RandomForest" ) {
     
-    test_object <- test_rf(genex_df_test = genex_df_this_sample,
-                           metadata_df_test = metadata_df_this_sample,
-                           classifier = classifier)
+    model_type <- "rf"
     
   } else {
     
-    test_object <- list(
-      prediction_labels_df = data.frame(),
-      model_output = data.frame()
-    )
+    stop("classifier model class must be OnevsrestScheme_TSP for kTSP or rule_based_RandomForest for random forest")
     
   }
   
-  if (length(unclassified_cell_ids) > 0) {
-    # build out a prediction matrix for the unclassified cells to append to
-    # test_object$model_output
-    pred_mat_unclassified <- data.frame(
-      cell_id = unclassified_cell_ids,
-      G3 = NA_real_,
-      G4 = NA_real_,
-      SHH = NA_real_,
-      WNT = NA_real_
-    ) |>
-      tibble::column_to_rownames("cell_id")
+  # Need rules data frame for kTSP
+  if (model_type == "ktsp" & is.null(rules_df)) {
     
-    # make a data frame of the unclassified cells to rbind to the test object
-    pred_unclassified_df <- tibble::tibble(
-      sample_accession = unclassified_cell_ids,
-      predicted_labels = "Unclassified"
-    )
+    stop("rules_df can not be NULL when model type is kTSP")
     
-    # tack on the unclassified cells
-    test_object$model_output <- data.frame(test_object$model_output) |>
-      dplyr::bind_rows(pred_mat_unclassified)
-    test_object$predicted_labels_df <- test_object$predicted_labels_df |>
-      dplyr::bind_rows(pred_unclassified_df)
   }
-
-  return(test_object)
-
-}
-
-test_ktsp_single_cells <- function(sample_acc,
-                                   sce_filepath,
-                                   metadata_df,
-                                   labels,
-                                   classifier,
-                                   rules_df,
-                                   prop_observed = 0.1,
-                                   filtering_type = "rule",
-                                   platform = "scRNA-seq"){
   
-  filtering_type <- tolower(filtering_type)
+  # Need vector of genes used in the classifier for RF
+  if (model_type == "rf" & is.null(genes_in_classifier)) {
+    
+    stop("genes_in_classifier can not be NULL when model type is kTSP")
+    
+  }
+  
   stopifnot(
-    "Classifier type not OnevsrestScheme_TSP" = (class(classifier) == "OnevsrestScheme_TSP"),
     "Unsupported filtering type" = (filtering_type %in% c("rule", "gene"))
   )
   
@@ -294,7 +200,9 @@ test_ktsp_single_cells <- function(sample_acc,
                                             subgroup = sample_subgroup,
                                             platform = platform)
   
-  if (filtering_type == "gene") {
+  # If filtering by proportion of genes observed, which is a type for kTSP and
+  # the only way to do it for RF
+  if (filtering_type == "gene" | model_type == "rf") {
     
     # Get a vector of genes used in the classifier
     genes_in_classifier <- rules_df |>
@@ -308,7 +216,7 @@ test_ktsp_single_cells <- function(sample_acc,
       (num_genes_detected / length(genes_in_classifier)) > prop_observed
     )
     
-  } else {  # Filtering type == "rule"
+  } else {  # Filtering type == "rule" & model is "ktsp"
     
     rules_prop_observed_df <- rules_df |>
       # Join with gene expression data
@@ -351,38 +259,91 @@ test_ktsp_single_cells <- function(sample_acc,
   check_input_files(genex_df = genex_df_this_sample,
                     metadata_df = metadata_df_this_sample)
   
-  # If at least one cell is retained, test as normal
-  if (ncol(genex_df_this_sample) > 0) {
+  if (model_type == "ktsp") {
     
-    test_object <- test_ktsp(genex_df_test = genex_df_this_sample,
+    # If there are cells to be classified, predict as normal
+    if (ncol(genex_df_this_sample) > 0) {
+      
+      test_object <- test_ktsp(genex_df_test = genex_df_this_sample,
+                               metadata_df_test = metadata_df_this_sample,
+                               classifier = classifier,
+                               labels = labels)
+  
+    } else {  # Otherwise, build out empty data frames
+      
+      test_object <- list(
+        prediction_labels_df = data.frame(),
+        model_output = data.frame()
+      )
+      
+    }
+    
+    # If there are any unclassified cells
+    if (length(unclassified_cell_ids) > 0) {
+      
+      # build out a prediction matrix for the unclassified cells to append
+      # to test_object$model_output
+      pred_mat_unclassified <- data.frame(
+        cell_id = unclassified_cell_ids,
+        G3 = NA_real_,
+        G4 = NA_real_,
+        SHH = NA_real_,
+        WNT = NA_real_,
+        max_score = "Unclassified",
+        tie_flag = NA_character_
+      ) |>
+        tibble::column_to_rownames("cell_id")
+      
+      # tack on the unclassified cells
+      test_object$model_output <- data.frame(test_object$model_output) |>
+        dplyr::bind_rows(pred_mat_unclassified)
+      
+    }
+    
+  } else if (model_type == "rf") {
+    
+    # If there are cells to be classified, predict as normal
+    if (ncol(genex_df_this_sample) > 0) {
+      
+      test_object <- test_rf(genex_df_test = genex_df_this_sample,
                              metadata_df_test = metadata_df_this_sample,
-                             classifier = classifier,
-                             labels = labels)
+                             classifier = classifier)
+      
+    } else {  # Otherwise, build out empty data frames
+      
+      test_object <- list(
+        prediction_labels_df = data.frame(),
+        model_output = data.frame()
+      )
+      
+    }
     
-  } else {  # Otherwise, initialize empty data frames
+    # If there are any unclassified cells
+    if (length(unclassified_cell_ids) > 0) {
+      
+      # build out a prediction matrix for the unclassified cells to append to
+      # test_object$model_output
+      pred_mat_unclassified <- data.frame(
+        cell_id = unclassified_cell_ids,
+        G3 = NA_real_,
+        G4 = NA_real_,
+        SHH = NA_real_,
+        WNT = NA_real_
+      ) |>
+        tibble::column_to_rownames("cell_id")
+      
+      # tack on the unclassified cells
+      test_object$model_output <- data.frame(test_object$model_output) |>
+        dplyr::bind_rows(pred_mat_unclassified)
+    }
+  } else {
     
-    test_object <- list(
-      prediction_labels_df = data.frame(),
-      model_output = data.frame()
-    )
+    stop("Unsupported model type!")  # Should be caught by now
     
   }
   
-  # If there are some cells that will not be classified
+  # If there are any unclassified cells
   if (length(unclassified_cell_ids) > 0) {
-    
-    # build out a prediction matrix for the unclassified cells to append
-    # to test_object$model_output
-    pred_mat_unclassified <- data.frame(
-      cell_id = unclassified_cell_ids,
-      G3 = NA_real_,
-      G4 = NA_real_,
-      SHH = NA_real_,
-      WNT = NA_real_,
-      max_score = "Unclassified",
-      tie_flag = NA_character_
-    ) |>
-      tibble::column_to_rownames("cell_id")
     
     # make a data frame of the unclassified cells to rbind to the test object
     pred_unclassified_df <- tibble::tibble(
@@ -390,14 +351,12 @@ test_ktsp_single_cells <- function(sample_acc,
       predicted_labels = "Unclassified"
     )
     
-    # tack on the unclassified cells
-    test_object$model_output <- data.frame(test_object$model_output) |>
-      dplyr::bind_rows(pred_mat_unclassified)
+    # add unclassified cells to predicted labels data frame
     test_object$predicted_labels_df <- test_object$predicted_labels_df |>
       dplyr::bind_rows(pred_unclassified_df)
-    
   }
   
   return(test_object)
-
+  
+  
 }
