e66d239c45735fe6a22cf2e87f59a0fe2bc896b0
Jaclyn Taroni <19534205+jaclyn-taroni@users.noreply.github.com>
2025-12-11 13:50:25 -0500

    Take genesets as arguments and rename

--- scripts/calculate_aucell.R
+++ scripts/calculate_aucell.R
@@ -1,27 +1,50 @@
 # J. Taroni
 # 2025
 #
-# Calculate AUCell scores for each set of human cerebellar developmental cell
-# types marker genes from Aldinger et al. 2021.
-# Marker genes are generated via extract_cbl_dev_marker_genes.R
+# Calculate AUCell scores for each geneset supplied in an RDS that is a
+# GSEABase::GeneSetCollection
+# Gene sets are generated/prepared in `scripts/prepare_marker_gene_sets.R`
 #
+# Usage:
+#   Rscript calculate_aucell.R \
+#     --geneset_file processed_data/single_cell/joshi_genesets.rds \
+#     --output_file plots/data/joshi_geneset_aucell_scores.tsv
 
 #### Libraries -----------------------------------------------------------------
 
 library(AUCell)
 library(SingleCellExperiment)
+library(optparse)
+
+#### Options and parsing -------------------------------------------------------
+
+option_list <- list(
+  make_option(
+    opt_str = c("-g", "--geneset_file"),
+    type = "character",
+    help = "File path for RDS file that contains genesets as "
+  ),
+  make_option(
+    opt_str = c("-o", "--output_file"),
+    type = "character",
+    help = "File path for TSV file that will be output with the results"
+  )
+)
+
+# Parse options
+opt <- parse_args(OptionParser(option_list = option_list))
 
 #### Functions -----------------------------------------------------------------
 
 calculate_aucell_scores <- function(sce,
                                     sample_title) {
   # Given a SingleCellExperiment object, return a data frame of AUCell scores
-  # for all genesets from the CBL dev marker genes
+  # for all genesets in the GSEABase::GeneSetCollection()
   # The first two columns will contain the sample_title and cell_index for
   # downstream plotting applications
 
-  # Run AUCell using the CBL dev gene sets
-  auc_results <- AUCell_run(sce, geneSets = cbl_dev_genesets)
+  # Run AUCell using the gene sets
+  auc_results <- AUCell_run(sce, geneSets = genesets)
 
   auc_df <- auc_results@assays@data$AUC |>
     # Transpose
@@ -70,18 +93,6 @@ if (any(duplicated(sce_sample_titles))) {
 
 }
 
-# CBL dev marker genes
-markers_genesets_file <- here::here(
-  single_cell_data_dir,
-  "aldinger_cbl_dev_genesets.rds"
-)
-
-# Output file for AUCell scores
-aucell_scores_file <- here::here(
-  plots_data_dir,
-  "individual_cells_cbl_dev_aucell_scores.tsv"
-)
-
 #### Read in data --------------------------------------------------------------
 
 # SingleCellExperiment from both datasets
@@ -89,7 +100,7 @@ sce_list <- sce_files |>
   purrr::map(readr::read_rds)
 
 # GeneSetCollection
-cbl_dev_genesets <- readr::read_rds(markers_genesets_file)
+genesets <- readr::read_rds(opt$genesets_file)
 
 # Data frame of AUCell scores for all genesets from all samples
 auc_df <- sce_list |>
@@ -99,5 +110,4 @@ auc_df <- sce_list |>
   dplyr::bind_rows()
 
 # Write AUCell scores to file
-readr::write_tsv(auc_df, file = aucell_scores_file)
-
+readr::write_tsv(auc_df, file = opt$output_file)
